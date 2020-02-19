50033a242e6e9d67c27330993443ff44e0f068f0
Evgeniy Ocheredko <eoch@jooble.com>
2020-02-19 13:59:35 +0200

    config for feedmaker, improved visualization for generation table

--- src/containers/FeedMaker/FeedMaker.js
+++ src/containers/FeedMaker/FeedMaker.js
@@ -1,47 +1,36 @@
 import React, { useState, useEffect } from "react";
+import { Icon, Typography }           from "antd";
 import LayoutWrapper                  from "@iso/components/utility/layoutWrapper.js";
 import PageHeader                     from '@iso/components/utility/pageHeader';
 import TableWrapper                   from "@iso/containers/Tables/AntTables/AntTables.styles.js";
 import Table                          from "@iso/components/uielements/table";
 import Progress                       from "@iso/components/uielements/progress";
 import Box                            from "@iso/components/utility/box";
-import Spin                           from "@iso/components/uielements/spin";
 import strCapitalize                  from "@iso/lib/stringCapitalize";
 import SuperFetch                     from "@iso/lib/helpers/superFetch";
 import socketIOClient                 from "socket.io-client";
 import GenerationForm                 from "@iso/components/FeedMaker/GenerationForm";
 import config                         from "@iso/config/feedmaker.config";
+import Hamster                        from "@iso/assets/images/hamster.gif"
 
+const { Title } = Typography;
 const { Column } = Table;
 
 
 export default () => {
+  const [io, setIo] = useState(null);
   const [loading, setLoading] = useState(true);
   const [genTypes, setGenTypes] = useState([]);
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const io = socketIOClient(config.apiUrl);
-    io.on("connect", data => console.log(data));
-    io.on("fetch_completed", data => console.log(data));
+    io.on("connect", generations => setData(generations), setLoading(false));
+    io.on("update", generations => setData(generations));
+    setIo(io);
+
     SuperFetch.get(`${ config.apiUrl }/gentypes`, true)
       .then(response => setGenTypes(response.data));
-    setData([
-      {
-        key: "google_ru",
-        gentype: "google_ru",
-        fetching: true,
-        processing: 63,
-        uploading: 24,
-      },
-      {
-        key: "yandex_ua",
-        gentype: "yandex_ua",
-        fetching: false,
-        processing: 29,
-        uploading: 12,
-      },
-    ]);
     setTimeout(setLoading, 1000, false)
   }, []);
 
@@ -58,33 +47,54 @@ export default () => {
         >
           <Column
             title="Generation Type"
-            dataIndex="gentype"
-            key="gentype"
+            dataIndex="key"
             width={ 30 }
-            render={ genType => strCapitalize(genType) }
+            render={ key => strCapitalize(key) }
+          />
+          <Column
+            title="Generation Date"
+            dataIndex="generation_date"
+            width={ 40 }
+            render={ generation_date =>
+              new Date(generation_date).toLocaleString().replace(',', '')
+            }
           />
           <Column
             title="Fetching from DB"
             dataIndex="fetching"
-            key="fetching"
             align="center"
             render={ fetching => fetching ?
-              <Spin spinning={ fetching } /> :
+              <img src={ Hamster } alt="#" style={ { width: 40 } } /> :
               <Progress type="circle" width={ 30 } percent={ 100 } />
             }
           />
           <Column
             title="Processing records"
             dataIndex="processing"
-            key="processing"
-            render={ processing => <Progress status="active" percent={ processing } /> }
+            render={ processing =>
+              <Progress
+                status={ processing === 100 ? "success" : "active" }
+                percent={ processing }
+              /> }
           />
           <Column
-            title="Uploading Files"
+            align="center"
+            title="Files Uploaded/Created"
             dataIndex="uploading"
-            key="uploading"
-            render={ uploading => <Progress status="active" percent={ uploading } /> }
+            render={ ({ created, uploaded }) =>
+              <Title level={ 4 } code={ true }>
+                { `${ uploaded }/${ created }` }
+              </Title>
+            }
           />
+          <Column render={ record =>
+            <Icon
+              style={ { fontSize: 20 } }
+              spin={ true }
+              type="delete"
+              onClick={ () => io.emit("delete_generation", { generation_type: record.key }) }
+            />
+          } />
         </TableWrapper>
       </Box>
     </LayoutWrapper>
