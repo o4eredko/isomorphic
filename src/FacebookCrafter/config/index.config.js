ffb6c1ff14e269635738ddae0b4f1617cd03d670
eoch <eoch@jooble.com>
2020-04-03 16:24:31 +0300

    Config for fb crafter

--- src/FacebookCrafter/config/index.config.js
+++ src/FacebookCrafter/config/index.config.js
@@ -1,5 +1,8 @@
 export default {
   httpUrl: "http://skyline.jo:8888/graphql/",
   wsUrl: "ws://skyline.jo:8888/graphql/",
-  requiredCsvFields: ["city", "adset_name", "ad_title", "ad_url", "creative_display_url"]
+  csvFields: {
+    required: ["city", "adset_name", "ad_title", "ad_url"],
+    nonRequired: ["creative_display_url"],
+  }
 }
